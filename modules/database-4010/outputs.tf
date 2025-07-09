8152388458c40eb2485547e966c1547f93371dde
DevinForr <devinrforrest@gmail.com>
2025-07-09 19:36:36 -0400

    fixed errors

--- modules/database-4010/outputs.tf
+++ modules/database-4010/outputs.tf
@@ -1,9 +1,9 @@
 output "postgres_server_name" {
   description = "Name of the PostgreSQL server"
-  value       = azurerm_postgresql_server.postgres.name
+  value       = azurerm_postgresql_flexible_server.postgres.name
 }
 
 output "postgres_fqdn" {
   description = "FQDN of the PostgreSQL server"
-  value       = azurerm_postgresql_server.postgres.fqdn
+  value       = azurerm_postgresql_flexible_server.postgres.fqdn
 }
