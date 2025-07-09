f8746426fa585920f0fe8ee6a3cc3d84632b1999
DevinForr <devinrforrest@gmail.com>
2025-07-09 15:44:41 -0400

    Fixed backend variable

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rgroup-4010"                     # Update to match backend RG
-    storage_account_name = "4010tfstatestore"            # Must exist
+    storage_account_name = "4010commonstore"            # Must exist
     container_name       = "tfstate"
     key                  = "assignment1-4010.tfstate"
   }
