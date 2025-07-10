85e08b260702c47eb9729b8bfc433bad4016d56c
DevinForr <devinrforrest@gmail.com>
2025-07-09 20:32:02 -0400

    fixed errors

--- main.tf
+++ main.tf
@@ -28,8 +28,6 @@ module "linux_vms" {
   location                 = local.location
   resource_group_name      = module.rgroup.resource_group_name
   subnet_id                = module.network.subnet_id
-  nsg_id                   = module.network.nsg_id
-  diagnostics_storage_uri  = module.common.storage_uri
   backend_pool_id         = module.loadbalancer.backend_pool_id
   linux_vms = {
     vm1 = module.linux_vms.vm_ids["vm1"]
